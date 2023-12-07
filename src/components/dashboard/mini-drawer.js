46b287d2aee2361a43acd116cb8492b45836f15b
Avantika Sathaye <avantikasathaye@Avantikas-MacBook-Pro-2.local>
2023-12-07 07:43:01 +0530

    WIP login

--- src/components/dashboard/mini-drawer.js
+++ src/components/dashboard/mini-drawer.js
@@ -18,6 +18,13 @@ import { brandKit } from '../../theme/colors';
 import { black } from '@mui/material/colors';
 import { useLocation } from 'react-router-dom';
 import ResponsiveSubDrawer from './ResponsiveSubDrawer';
+import SemaIcon from '../../assets/LogoSema02.png'
+import PersonIcon from '@mui/icons-material/Person';
+import HelpIcon from '@mui/icons-material/Help';
+import { Button } from '@mui/material';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Modal from '@mui/material/Modal';
 
 const drawerWidth = 240;
 
@@ -102,6 +109,18 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
   }),
 );
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
 export default function MiniDrawer ({children}){
   //const pathname = usePathname();
   let location = useLocation();
@@ -116,28 +135,90 @@ export default function MiniDrawer ({children}){
     setOpen(false);
   };
 
+  const [anchorEl, setAnchorEl] = useState(null);
+  const openMenu = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const [openModal, setOpenModal] = useState(false);
+  const handleOpenModal = () => setOpenModal(true);
+  const handleCloseModal = () => setOpenModal(false);
+
   return (
     <ThemeProvider theme={mytheme}>
     <Box sx={{ display: 'flex' }} >
       <CssBaseline />
-      {/* <ResponsiveSubDrawer /> */}
-      <AppBar position="fixed" open={open} >
+      <AppBar position="fixed" open={open} style={{color: "black"}}>
         <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            sx={{
-              marginRight: 15,
-              ...(open && { display: 'none' }),
-            }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            Semamart Procurement Software
-          </Typography>
+          <div className='leftMenuItems'>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              sx={{
+                ...(open && { display: 'none' }),
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <img
+                src={SemaIcon}
+                alt="Icon"
+                className="rounded mr-2 brandName"
+                width="30"
+                height="20"
+                style={{marginBottom: "5px", marginRight: "5px"}}
+                  />{" "}
+            <Typography variant="h6" noWrap component="div">
+              Semamart Procurement Software
+            </Typography>
+          </div>
+          
+          <div className='rightMenuItems' style={{display:'flex'}}>
+            <Button style={{color: 'black'}} onClick={handleOpenModal}><HelpIcon /></Button>
+            <Modal
+              open={openModal}
+              onClose={handleCloseModal}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <Box sx={style}>
+                <Typography id="modal-modal-title" variant="h6" component="h2" style={{backgroundColor: brandKit.topNav}}>
+                  FAQs
+                </Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                  Show Semamart Procurement Software FAQs here ! 
+                </Typography>
+              </Box>
+            </Modal>
+            
+            <Button style={{color: 'black'}} 
+              id="basic-button"
+              aria-controls={openMenu ? 'basic-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={openMenu ? 'true' : undefined}
+              onClick={handleClick}> <PersonIcon /> 
+            </Button>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={openMenu}
+              onClose={handleClose}
+              MenuListProps={{
+                'aria-labelledby': 'basic-button',
+              }}
+            >
+              <MenuItem onClick={handleClose}>User Profile</MenuItem>
+              <MenuItem onClick={handleClose}>User Settings</MenuItem>
+              <MenuItem onClick={handleClose}>Logout</MenuItem>
+            </Menu>
+          </div>
+          
         </Toolbar>
       </AppBar>
       <Drawer variant="permanent" open={open} >
