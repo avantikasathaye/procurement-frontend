32b90135201faaa947baeee40e990239d9975703
Avantika Sathaye <avantika.sathaye@innovyatech.com>
2023-12-07 13:32:13 +0530

    Login screen added

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import Products from './components/sections/Products/Products';
 import MiniDrawer from './components/dashboard/mini-drawer';
 import SupplyChain from './components/sections/SupplyChain/SupplyChain';
 import ResponsiveSubDrawer from './components/dashboard/ResponsiveSubDrawer';
+import Login from './screens/Login';
 
 function App() {
   return (
     <BrowserRouter>
     <MiniDrawer>
       <Routes>
-        <Route path="/products" element={<Products />} />
-        <Route path="/supply-chain" element={<SupplyChain />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/products" element={<Products />} />
+          <Route path="/supply-chain" element={<SupplyChain />}/>
       </Routes>
     </MiniDrawer>
   </BrowserRouter>
